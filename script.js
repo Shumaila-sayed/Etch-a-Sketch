3874cebf68ecece495d4f15591cdb59233178c3c
Shumaila <sayedshumaila27@gmail.com>
2024-08-01 16:11:16 +0530

    dynamically change the number of div depending upon the user input

--- script.js
+++ script.js
@@ -9,5 +9,29 @@ for (let i = 0; i < 16*16 ; i++) {
 })
 }
 
+let btn = document.createElement('BUTTON');
+btn.className = "btn";
+document.body.insertBefore(btn, container);
+btn.textContent = "Change Grid Size";
 
+   function reSize() {
+       let num = prompt('Enter size: (not more than 100)');
+       
+       if(num > 100){
+        alert('Not over 100!!!');
+        return;
+       } else {
+        container.replaceChildren();
+        for (let i = 0; i < num*num ; i++) {
+            let smallDiv = document.createElement('div');
+            smallDiv.className  = 'small-div';         
+            smallDiv.style.height = (600/num) + 'px';
+            smallDiv.style.width =  (600/num) + 'px';
+            container.appendChild(smallDiv);
+            smallDiv.addEventListener('mouseenter', () => {
+            smallDiv.style.backgroundColor = "red";
+        })
+  }}
+}
+btn.addEventListener('click', reSize);
 
