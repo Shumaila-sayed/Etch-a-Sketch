85b0963eb41a084b64a9793bbd7c54974dbb0c4d
Shumaila <sayedshumaila27@gmail.com>
2024-08-01 16:25:22 +0530

    add random color to div

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let container = document.getElementById('container');
 
+function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+  }
+
 for (let i = 0; i < 16*16 ; i++) {
     let smallDiv = document.createElement('div');
     smallDiv.className  = 'small-div';
     container.appendChild(smallDiv);
     smallDiv.addEventListener('mouseenter', () => {
-    smallDiv.style.backgroundColor = "red";
+    smallDiv.style.backgroundColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
 })
 }
 
@@ -16,7 +20,7 @@ btn.textContent = "Change Grid Size";
 
    function reSize() {
        let num = prompt('Enter size: (not more than 100)');
-       
+
        if(num > 100){
         alert('Not over 100!!!');
         return;
@@ -29,7 +33,7 @@ btn.textContent = "Change Grid Size";
             smallDiv.style.width =  (600/num) + 'px';
             container.appendChild(smallDiv);
             smallDiv.addEventListener('mouseenter', () => {
-            smallDiv.style.backgroundColor = "red";
+            smallDiv.style.backgroundColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
         })
   }}
 }
